3bd900c3f79097e0e8ba8a47b7c205e5404bdb7e
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-01-23 13:59:48 +0100

    Added next-auth providers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,15 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import AppleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db";
+
+console.log(env);
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -19,10 +24,35 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+    // FacebookProvider({
+    //   clientId: env.FACEBOOK_CLIENT_ID,
+    //   clientSecret: env.FACEBOOK_CLIENT_SECRET,
+    // }),
+    // AppleProvider({
+    //   clientId: env.APPLE_ID,
+    //   clientSecret: env.APPLE_SECRET,
+    // }),
+    // CredentialsProvider({
+    //   id: "credentials",
+    //   name: "credentials",
+    //   credentials: {
+    //     email: {
+    //       label: "Email",
+    //       type: "text",
+    //       placeholder: "example@email.com",
+    //     },
+    //     password: {
+    //       label: "Password",
+    //       type: "password",
+    //       placeholder: "Your password",
+    //     },
+    //   },
+    //   authorize: () => null,
+    // }),
     /**
      * ...add more providers here
      *
