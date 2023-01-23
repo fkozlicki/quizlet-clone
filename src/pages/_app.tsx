685b64796500762715036c7a549d8fe54c544e85
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-01-23 14:00:14 +0100

    Wrapped app in Layout and AuthFormContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import Layout from "../components/Layout";
+import AuthFormProvider from "../contexts/AuthFormContext";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +14,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <AuthFormProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthFormProvider>
     </SessionProvider>
   );
 };
