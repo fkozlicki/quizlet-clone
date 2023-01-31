d60876d737f0d0c1ecc74dfd29dd8a6deab09896
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-01-31 19:40:09 +0100

    Added animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,29 @@ module.exports = {
     extend: {
       transitionProperty: {
         height: "height",
+        heightWidth: "height, width",
       },
       transitionTimingFunction: {
         borderHeight: "cubic-bezier(0.47, 0, 0.745, 0.715)",
+      },
+      keyframes: {
+        flipIn: {
+          "0%": { transform: "rotateX(0deg)" },
+          "100%": { transform: "rotateX(180deg)" },
+        },
+        flipOut: {
+          "0%": { transform: "rotateX(180deg)" },
+          "100%": { transform: "rotateX(360deg)" },
+        },
+        mismatch: {
+          "0%": { background: "red" },
+          "100%": { background: "white" },
+        },
+      },
+      animation: {
+        flipIn: "flipIn .25s ease-in-out 1",
+        flipOut: "flipOut .25s ease-in-out 1",
+        mismatch: "mismatch 1s ease-in-out 1",
       },
     },
   },
