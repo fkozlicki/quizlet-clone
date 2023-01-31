8be8867efca12e65b46c8600c824489c74ccaf98
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-01-31 19:42:34 +0100

    Cleared console .log

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,8 +9,6 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db";
 
-console.log(env);
-
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
