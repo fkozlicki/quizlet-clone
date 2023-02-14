d997a41fdfdc40bbd7d7d8bad0526e59b9e2b211
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-14 15:47:32 +0100

    Updated Layout component - MobileMenu and AuthDropdown functionality

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,65 @@
-import React, { type PropsWithChildren } from "react";
-import { useAuthFormContext } from "../contexts/AuthFormContext";
+import { useRouter } from "next/router";
+import React, { useEffect, useState, type PropsWithChildren } from "react";
+import { Toaster } from "react-hot-toast";
+import AuthDropdown from "./AuthDropdown";
+import MobileMenu from "./MobileMenu";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }: PropsWithChildren) => {
-  const [authForm] = useAuthFormContext();
+  const [authDropdownOpen, setAuthDropdownOpen] = useState<
+    "signup" | "login" | false
+  >(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    closeMobileMenu();
+  }, [pathname]);
+
+  useEffect(() => {
+    if (mobileMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [mobileMenuOpen]);
+
+  const openSignup = () => {
+    setAuthDropdownOpen("signup");
+  };
+  const openLogin = () => {
+    setAuthDropdownOpen("login");
+  };
+  const closeAuthDropdown = () => {
+    setAuthDropdownOpen(false);
+  };
+  const openMobileMenu = () => {
+    setMobileMenuOpen(true);
+  };
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
     <div
       className={`relative ${
-        authForm === "closed" ? "" : "h-screen overflow-hidden"
+        authDropdownOpen === false ? "" : "h-screen overflow-hidden"
       }`}
     >
-      <Navbar />
+      <Toaster position="top-center" />
+      <Navbar
+        openSignup={openSignup}
+        openLogin={openLogin}
+        openMobileMenu={openMobileMenu}
+      />
       {children}
+      <AuthDropdown
+        status={authDropdownOpen}
+        openSignup={openSignup}
+        openLogin={openLogin}
+        close={closeAuthDropdown}
+      />
+      <MobileMenu status={mobileMenuOpen} close={closeMobileMenu} />
     </div>
   );
 };
