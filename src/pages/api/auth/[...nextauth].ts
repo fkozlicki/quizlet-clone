ef761f981162fb6306ae1868b996a6aa3182ebf4
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-14 15:36:07 +0100

    Added Facebook and Credentials providers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,49 +8,78 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db";
+import { compare } from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
       }
+
       return session;
     },
   },
   // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    // FacebookProvider({
-    //   clientId: env.FACEBOOK_CLIENT_ID,
-    //   clientSecret: env.FACEBOOK_CLIENT_SECRET,
-    // }),
+    FacebookProvider({
+      clientId: env.FACEBOOK_CLIENT_ID,
+      clientSecret: env.FACEBOOK_CLIENT_SECRET,
+    }),
     // AppleProvider({
     //   clientId: env.APPLE_ID,
     //   clientSecret: env.APPLE_SECRET,
     // }),
-    // CredentialsProvider({
-    //   id: "credentials",
-    //   name: "credentials",
-    //   credentials: {
-    //     email: {
-    //       label: "Email",
-    //       type: "text",
-    //       placeholder: "example@email.com",
-    //     },
-    //     password: {
-    //       label: "Password",
-    //       type: "password",
-    //       placeholder: "Your password",
-    //     },
-    //   },
-    //   authorize: () => null,
-    // }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@email.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "*****",
+        },
+      },
+      authorize: async (credentials) => {
+        if (!credentials) return null;
+
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        if (!user) return null;
+
+        if (!user.password) return null;
+
+        const isValidPassword = await compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValidPassword) return null;
+
+        return user;
+      },
+    }),
     /**
      * ...add more providers here
      *
@@ -61,6 +90,7 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  adapter: PrismaAdapter(prisma),
 };
 
 export default NextAuth(authOptions);
