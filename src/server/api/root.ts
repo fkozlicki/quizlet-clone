69cb42c928f3284adf97ed07668b7dc9a829de68
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-14 15:37:12 +0100

    Updated app router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import { studySetRouter } from "./routers/study-set";
+import { userRouter } from "./routers/user";
+import { cardRouter } from "./routers/card";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,9 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  studySet: studySetRouter,
+  user: userRouter,
+  card: cardRouter,
 });
 
 // export type definition of API
