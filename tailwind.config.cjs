3f9e4872ca1354641ec4f3b25ca8df62fb7f7d68
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-14 15:34:41 +0100

    Created animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,15 +19,77 @@ module.exports = {
           "0%": { transform: "rotateX(180deg)" },
           "100%": { transform: "rotateX(360deg)" },
         },
-        mismatch: {
-          "0%": { background: "red" },
-          "100%": { background: "white" },
+        shake: {
+          "0%": { transform: "rotate(-2deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
+        slideLeft: {
+          "0%": {
+            transform: "perspective(1000px) rotateY(15deg) translateX(-60px)",
+          },
+          "100%": {
+            transform: "perspective(1000px) rotateY(0deg) translateX(0px)",
+          },
+        },
+        slideRight: {
+          "0%": {
+            transform: "perspective(1000px) rotateY(-15deg) translateX(60px)",
+          },
+          "100%": {
+            transform: "perspective(1000px) rotateY(0deg) translateX(0px)",
+          },
+        },
+        learning: {
+          "0%": {
+            opacity: 0,
+            visibility: "hidden",
+          },
+          "25%": {
+            opacity: 1,
+            visibility: "visible",
+            transform: "rotate(2deg)",
+          },
+          "75%": {
+            opacity: 1,
+            visibility: "visible",
+            transform: "rotate(2deg)",
+          },
+          "100%": {
+            opacity: 0,
+            visibility: "hidden",
+            transform: "rotate(0deg) translateX(-50px)",
+          },
+        },
+        know: {
+          "0%": {
+            opacity: 0,
+            visibility: "hidden",
+          },
+          "25%": {
+            opacity: 1,
+            visibility: "visible",
+            transform: "rotate(-2deg)",
+          },
+          "75%": {
+            opacity: 1,
+            visibility: "visible",
+            transform: "rotate(-2deg)",
+          },
+          "100%": {
+            opacity: 0,
+            visibility: "visible",
+            transform: "rotate(0deg) translateX(50px)",
+          },
         },
       },
       animation: {
         flipIn: "flipIn .25s ease-in-out 1",
         flipOut: "flipOut .25s ease-in-out 1",
-        mismatch: "mismatch 1s ease-in-out 1",
+        slideLeft: "slideLeft .15s linear 1",
+        slideRight: "slideRight .15s linear 1",
+        shake: "shake 100ms ease-in-out 3",
+        learning: "learning .5s ease-in-out 1",
+        know: "know .5s ease-in-out 1",
       },
     },
   },
