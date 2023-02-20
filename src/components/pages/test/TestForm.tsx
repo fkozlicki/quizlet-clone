b2a1dc0d037152aa2736e88c24ef085f0ef7eeac
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-20 16:34:37 +0100

    Created TestForm component

---
+++ src/components/pages/test/TestForm.tsx
@@ -0,0 +1,103 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+import type { StudySetTest } from "../../../server/api/routers/study-set";
+import MultipleChoice from "../../cards/MultipleChoice";
+import TrueOrFalse from "../../cards/TrueOrFalse";
+import Written from "../../cards/Written";
+
+const flashcard = z.object({
+  id: z.string(),
+  term: z.string(),
+  definition: z.string(),
+  studySetId: z.string(),
+  userAnswer: z.string().min(1),
+});
+
+const trueOrFalse = z
+  .object({
+    answer: z.string(),
+  })
+  .merge(flashcard);
+
+const multipleChoice = z
+  .object({
+    answers: z.array(z.string()),
+  })
+  .merge(flashcard);
+
+const testInputs = z.object({
+  trueOrFalse: z.array(trueOrFalse),
+  multipleChoice: z.array(multipleChoice),
+  written: z.array(flashcard),
+});
+
+export type TestInputs = z.infer<typeof testInputs>;
+
+interface TestFormProps {
+  studySetTest: StudySetTest;
+  formCallack: (data: TestInputs) => void;
+}
+
+const TestForm = ({ studySetTest, formCallack }: TestFormProps) => {
+  const initialData = Object.fromEntries(
+    Object.entries(studySetTest).map((entry) => [
+      entry[0],
+      entry[1].map((question) => ({ ...question, userAnswer: "" })),
+    ])
+  );
+
+  const { control, register, handleSubmit } = useForm<TestInputs>({
+    // resolver: zodResolver(testInputs),
+    defaultValues: initialData,
+  });
+  const { fields: multipleChoice } = useFieldArray({
+    control,
+    name: "multipleChoice",
+  });
+  const { fields: trueOrFalse } = useFieldArray({
+    control,
+    name: "trueOrFalse",
+  });
+  const { fields: written } = useFieldArray({ control, name: "written" });
+
+  return (
+    <form onSubmit={handleSubmit(formCallack)} className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6">
+        {trueOrFalse.map(({ answer, term }, index) => (
+          <TrueOrFalse
+            key={index}
+            term={term}
+            answer={answer}
+            index={index}
+            register={register(`trueOrFalse.${index}.userAnswer`)}
+          />
+        ))}
+        {multipleChoice.map(({ term, answers }, index) => (
+          <MultipleChoice
+            key={index}
+            term={term}
+            answers={answers}
+            register={register(`multipleChoice.${index}.userAnswer`)}
+            index={index}
+            type="radio"
+          />
+        ))}
+        {written.map(({ term }, index) => (
+          <Written
+            key={index}
+            term={term}
+            register={register(`written.${index}.userAnswer`)}
+            index={index}
+          />
+        ))}
+      </div>
+      <button type="submit" className="rounded-md bg-blue-600 py-2 text-white">
+        Send anwers
+      </button>
+    </form>
+  );
+};
+
+export default TestForm;
