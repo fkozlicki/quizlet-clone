4321d126bb3a9b7262ef2eceb2168781ea790554
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-20 20:47:54 +0100

    Created study-sets page

--- src/pages/[id]/study-sets.tsx
+++ src/pages/[id]/study-sets.tsx
@@ -1,7 +1,10 @@
 import type { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import React from "react";
 import ProfileLayout from "../../components/layout/ProfileLayout";
+import StudySetPreview from "../../components/StudySetPreview";
+import { api } from "../../utils/api";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
@@ -31,9 +34,46 @@ interface StudySetsProps {
 }
 
 const StudySets = ({ achivements }: StudySetsProps) => {
+  const { query, isReady } = useRouter();
+
+  if (!isReady) return <div>Loading...</div>;
+
+  const userId = query.id as string;
+
+  const {
+    data: studySets,
+    isLoading,
+    error,
+  } = api.studySet.getUserSets.useQuery({
+    id: userId,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>{error.message}</div>;
+
   return (
     <ProfileLayout achivements={achivements}>
-      <div>StudySets</div>
+      <div>
+        {studySets.length > 0 ? (
+          <div>
+            <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
+              {studySets.map((set) => (
+                <StudySetPreview
+                  key={set.id}
+                  authorImage={set.user.image}
+                  authorName={set.user.name}
+                  title={set.title}
+                  termsCount={set.cards.length}
+                  id={set.id}
+                />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="">This user doesn&apos;t have any sets</div>
+        )}
+      </div>
     </ProfileLayout>
   );
 };
