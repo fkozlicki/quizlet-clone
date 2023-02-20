576c66f5fc91aa19840031cd5f05469bee28c027
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-20 16:38:51 +0100

    Moved Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { GetServerSideProps } from "next";
 import { type NextPage } from "next";
 import { getSession } from "next-auth/react";
-import Hero from "../components/Hero";
+import Hero from "../components/pages/home/Hero";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
