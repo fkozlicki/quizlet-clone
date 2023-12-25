de1be580a272068aa4afbc2b35d3fa0fd8444bd9
Philip Kozlicki <philip.kozlicki@gdsoft.info>
2023-12-25 17:56:19 +0100

    Refactored image uploading

--- src/components/pages/settings/EditProfilePicture.tsx
+++ src/components/pages/settings/EditProfilePicture.tsx
@@ -1,50 +1,46 @@
-import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  InboxOutlined,
+  LoadingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import type { User } from "@prisma/client";
 import { useMutation } from "@tanstack/react-query";
+import { Avatar, Divider, Upload, message } from "antd";
+import type { RcFile } from "antd/es/upload";
 import Image from "next/image";
-import React, { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
-import { z } from "zod";
+import { useState } from "react";
 import pictures from "../../../data/pictures.json";
 import { api } from "../../../utils/api";
-import ProfileImage from "../../ProfileImage";
 
 interface EditProfilePictureProps {
   image: User["image"];
-  userName: User["name"];
 }
-
-const editPictureSchema = z.object({
-  files: typeof window === "undefined" ? z.null() : z.instanceof(FileList),
-});
-
-type EditPictureInputs = z.infer<typeof editPictureSchema>;
 
 const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dkg9zehpc/image/upload";
 const CLOUDINARY_UPLOAD_PRESET = "quizletv2";
 
-const EditProfilePicture = ({ image, userName }: EditProfilePictureProps) => {
-  const submitButton = useRef<HTMLButtonElement>(null);
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<EditPictureInputs>({
-    resolver: zodResolver(editPictureSchema),
-    defaultValues: {},
-  });
+const beforeUpload = (file: RcFile) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    void message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    void message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
+
+const EditProfilePicture = ({ image }: EditProfilePictureProps) => {
   const [picture, setPicture] = useState<User["image"]>(image);
-  const pictureMutation = api.user.editPicture.useMutation({
-    onSuccess: ({ image }) => {
-      setPicture(image);
-      toast("Updated profile picture");
-    },
-    onError: () => {
-      toast("Couldn't update profile picture");
-    },
-  });
-  const uploadToCloudinary = useMutation({
+  const { mutate: updateUser, isLoading: updateLoading } =
+    api.user.update.useMutation({
+      onSuccess: ({ image }) => {
+        setPicture(image);
+        void message.success("Uploaded successfully");
+      },
+    });
+  const { mutate: uploadImage, isLoading: uploadLoading } = useMutation({
     mutationFn: async (data: FormData) => {
       const res = await fetch(CLOUDINARY_URL, {
         method: "POST",
@@ -53,78 +49,63 @@ const EditProfilePicture = ({ image, userName }: EditProfilePictureProps) => {
       return res.json() as Promise<{ secure_url: string }>;
     },
     onSuccess: (data) => {
-      updatePicture(data.secure_url);
-    },
-    onError: () => {
-      toast("Couldn't update profile picture");
+      updateUser({ image: data.secure_url });
     },
   });
 
-  const updatePicture = (image: string) => {
-    pictureMutation.mutate({ image });
-  };
-
-  const uploadPicture = (data: EditPictureInputs) => {
-    const newFile = data.files?.[0];
-    const formData = new FormData();
-    formData.append("file", newFile!);
-    formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
-
-    uploadToCloudinary.mutate(formData);
-  };
-
   return (
     <div className="mb-8 flex flex-col lg:flex-row lg:items-center lg:gap-8">
-      <div className="mb-4 flex items-center gap-4 lg:basis-48 lg:flex-col">
-        <ProfileImage
-          image={picture}
-          userName={userName}
-          size={64}
-          fontSize={20}
+      <div className="flex items-center gap-4 lg:basis-48 lg:flex-col">
+        <Avatar
+          icon={<UserOutlined />}
+          src={picture}
+          alt=""
+          className="h-16 w-16"
         />
         <div className="text-xl font-semibold">Profile Picture</div>
       </div>
       <div className="flex-1 rounded-lg bg-white p-4 shadow">
-        <div>Choose your profile picture</div>
+        <div className="mb-2">Choose your profile picture</div>
         <div className="flex flex-wrap gap-2">
           {pictures.map((picture, index) => (
-            <button
-              onClick={() => updatePicture(`/profile/${picture}`)}
+            <Image
               key={index}
-            >
-              <Image
-                src={`/profile/${picture}`}
-                alt=""
-                width={48}
-                height={48}
-                className="rounded-full border"
-              />
-            </button>
+              onClick={() => updateUser({ image: `/profile/${picture}` })}
+              src={`/profile/${picture}`}
+              alt=""
+              width={48}
+              height={48}
+              className="cursor-pointer rounded-full border"
+            />
           ))}
         </div>
-        <div className="mb-4 flex items-center">
-          <div className="h-px flex-1 bg-gradient-to-l from-slate-300" />
-          <div className="mx-2">or</div>
-          <div className="h-px flex-1 bg-gradient-to-r from-slate-300" />
-        </div>
-        <form onSubmit={handleSubmit(uploadPicture)}>
-          <label
-            htmlFor="profile-picture"
-            className="m-auto block w-max cursor-pointer rounded-md bg-cyan-400 px-4 py-2 font-medium text-white hover:bg-cyan-500"
-          >
-            Upload your own picture
-            <input
-              {...register("files", {
-                onChange: () => submitButton.current?.click(),
-              })}
-              type="file"
-              id="profile-picture"
-              className="hidden"
-            />
-          </label>
-          <button type="submit" ref={submitButton} className="hidden" />
-          {errors.files && <p>{errors.files.message}</p>}
-        </form>
+        <Divider className="text-sm text-gray-400">OR</Divider>
+        <Upload.Dragger
+          name="file"
+          multiple={false}
+          showUploadList={false}
+          customRequest={(options) => {
+            const data = new FormData();
+            data.append("file", options.file);
+            data.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+            uploadImage(data);
+          }}
+          beforeUpload={beforeUpload}
+          disabled={updateLoading || uploadLoading}
+        >
+          <p className="ant-upload-drag-icon">
+            {uploadLoading || updateLoading ? (
+              <LoadingOutlined />
+            ) : (
+              <InboxOutlined />
+            )}
+          </p>
+          <p className="ant-upload-text">
+            {uploadLoading || updateLoading
+              ? "Uploading image"
+              : "Click or drag file to this area to upload"}
+          </p>
+        </Upload.Dragger>
       </div>
     </div>
   );
