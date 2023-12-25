913b11959bdba6a160e70e1b3ff6d0755417c703
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-12-25 21:57:12 +0100

    Removed Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import { NextSeo } from "next-seo";
-import Hero from "../components/pages/home/Hero";
+import Image from "next/image";
+import { useAuthDropdownContext } from "../contexts/AuthDropdownContext";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
@@ -21,10 +22,40 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const Home = () => {
+  const [, dispatch] = useAuthDropdownContext();
+
   return (
     <>
       <NextSeo title="Quizlet 2.0" />
-      <Hero />
+      <div className="relative h-[calc(100vh-65px)] w-full">
+        <Image
+          src="/hero.jpg"
+          alt="Hero image"
+          fill
+          className="object-cover object-[20%]"
+        />
+        <div className="absolute h-full w-full bg-black/50"></div>
+        <div className="absolute left-0 top-0 h-full w-full p-7 md:px-7 md:py-16 lg:px-20 2xl:px-[10rem] 2xl:py-20">
+          <div className="flex h-full w-full flex-col items-start justify-start md:justify-center">
+            <div className="mx-4 my-6 max-w-[25rem] sm:m-8 md:m-12 lg:max-w-[37rem]">
+              <h1 className="text-4xl font-bold leading-tight text-white sm:text-5xl">
+                The best digital flashcards and study tools
+              </h1>
+              <p className="mt-4 text-white md:text-xl">
+                Join over 60 million students using Quizlet’s science-backed
+                flashcards, practice tests and expert solutions to improve their
+                grades and reach their goals.
+              </p>
+            </div>
+            <button
+              onClick={() => dispatch("openSignup")}
+              className="mb-12 ml-4 rounded-lg bg-blue-600 px-6 py-3 text-white hover:bg-blue-700  sm:ml-8 md:ml-12"
+            >
+              Sign up for free
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
