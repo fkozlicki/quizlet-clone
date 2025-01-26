1ef2e70ee78ae2ccd08d75b09705f12fa7cfeaf7
Filip Koźlicki <filip.kozlickii@gmail.com>
2025-01-26 11:22:45 +0100

    New monorepo config

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -20,9 +20,6 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  experimental: {
-    serverComponentsExternalPackages: ["pg"],
-  },
 };
 
 export default config;
